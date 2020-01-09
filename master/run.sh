4187147197bf04fd34f29360444134f8505581d7
Digital Nerd <33043683+digitalnerd@users.noreply.github.com>
2020-01-09 15:27:17 +0300

    Create run.sh

---
+++ master/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in $(ls -1 Tests | grep jmx)
+do
+    echo "*** Testing a ${i} file"
+    jmeter -n -t Tests/${i} -l Results/${i}-results.jtl
+    cat Results/${i}-results.jtl
+done
+
+sleep 10
